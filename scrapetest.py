9c38c4a7aea7ce31ed4e5bd71ff1a42f7ef38091
aakashvarma18 <aakashvarma18@gmail.com>
2018-03-27 05:18:38 +0530

    All links from main website are scrapped

--- scrapetest.py
+++ scrapetest.py
@@ -10,9 +10,12 @@ soup = BeautifulSoup(page.text, 'html.parser')
 article_tags = soup.find("div", {"id": "content"})
 # print (article_tags)
 article_links = article_tags.find_all(class_="fade")
-print (article_links)
+# print (article_links)
 
-
+for a in article_links:
+    # print (a.contents)
+    links = a.get('href')
+    print (links)
 
 
 
