070b029e1f7cee7f6ae0ef9e707cc3966d875d6a
aakashvarma18 <aakashvarma18@gmail.com>
2018-03-27 05:08:50 +0530

    filtered

--- scrapetest.py
+++ scrapetest.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = "https://www.coindesk.com/"
 page = requests.get(url)
@@ -8,8 +9,8 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 article_tags = soup.find("div", {"id": "content"})
 # print (article_tags)
-artist_name_list_items = article_tags.find_all(class_="fade")
-print (artist_name_list_items)
+article_links = article_tags.find_all(class_="fade")
+print (article_links)
 
 
 
